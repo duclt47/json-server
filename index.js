9e18426afdfd9d7b3796f552e57a46b437da4b20
Duc Le <duc.le@opswat.com>
2022-03-27 12:32:41 +0700

    update heroku port

--- index.js
+++ index.js
@@ -31,5 +31,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use('/api/card', cardRouter)
 
-const PORT = 5000;
-app.listen(5000, () => console.log(`Server started on 5000: 5000`))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}: ${PORT}`))
